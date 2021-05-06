8114c18eea3ed0ad901aeb8d11891d55732242d9
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-05-06 20:04:53 +0300

    Сервер

---
+++ server/server.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+class ConnectionManager:
+    def __init__(self):
+        self.connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.connections.append(websocket)
+
+    async def broadcast(self, data: str):
+        for connection in self.connections:
+            await connection.send_text(data)
+
+manager = ConnectionManager()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    while True:
+        try:
+            data = await websocket.receive_text()
+            await manager.broadcast(data)
+        except:
+            break
